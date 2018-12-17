93d27c8ee38d4cb3dafe4000c816ae87f39a27ae
zinovyevaleks <zinovyevaleks@gmail.com>
2018-12-17 16:02:57 +0700

    переделка форм на сайте

--- app/js/common.js
+++ app/js/common.js
@@ -22,4 +22,21 @@ $(function() {
           $('#audit_site_img').addClass('show');
       }
     });
+
+    //E-mail Ajax Send
+	$("form").submit(function() {
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "localhost:8888/mail.php",
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 });
