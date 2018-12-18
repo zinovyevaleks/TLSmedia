e061053c0803d5d5fed1661740f2e51b82c0eefc
zinovyevaleks <zinovyevaleks@gmail.com>
2018-12-18 13:13:19 +0700

    Настроил формы, мелкие правки
    Форма выбора услуг
    Скачивание прайса

--- app/js/common.js
+++ app/js/common.js
@@ -24,14 +24,32 @@ $(function() {
     });
 
     //E-mail Ajax Send
-	$("form").submit(function() {
+    //Форма заказа
+	$("#order").submit(function() {
 		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "localhost:8888/mail.php",
+			url: "http://localhost:8888/mail.php",
 			data: th.serialize()
 		}).done(function() {
 			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+    //Скачать прайс
+    $("#downloadPrice").submit(function() {
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:8888/mail.php",
+			data: th.serialize()
+		}).done(function() {
+			// alert("Thank you!");
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
