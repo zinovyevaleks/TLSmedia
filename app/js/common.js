dcd96ea60f64129530bd8127b4cf17533282a70f
zinovyevaleks <zinovyevaleks@gmail.com>
2019-01-22 23:49:23 +0700

    Подключил плагин Revealator Настроил анимации при прокрутке Заменил интерактивную карту

--- app/js/common.js
+++ app/js/common.js
@@ -1,115 +1,109 @@
 $(function() {
 
-	// Custom JS
-
-
+    // Custom JS
 
     //Плавная прокрутка до якоря
-    $(".anchor_link").click(function(){
-                var _href = $(this).attr("href");
-                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-                return false;
+    $(".anchor_link").click(function() {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
         });
+        return false;
+    });
 
-        // Переключение цветов активных хэдеров карточек в Аудите
+    // Переключение цветов активных хэдеров карточек в Аудите
 
-  $('.audit_card_header').click(function() {
-      if ($(this).hasClass('header-red')) {
-          $(this).removeClass('header-red');
-      }else{
-          $('.audit_card_header').removeClass('header-red');
-          $(this).addClass('header-red');
-      }
+    $('.audit_card_header').click(function() {
+        if ($(this).hasClass('header-red')) {
+            $(this).removeClass('header-red');
+        } else {
+            $('.audit_card_header').removeClass('header-red');
+            $(this).addClass('header-red');
+        }
 
-      if ($('#audit_ad').hasClass('header-red')) {
-          $('.tab-pane').removeClass('show');
-          $('#audit_ad_img').addClass('show');
-      }else if ($('#audit_sales').hasClass('header-red')) {
-          $('.tab-pane').removeClass('show');
-          $('#audit_sales_img').addClass('show');
-      }else if($('#audit_site').hasClass('header-red')) {
-          $('.tab-pane').removeClass('show');
-          $('#audit_site_img').addClass('show');
-      }
+        if ($('#audit_ad').hasClass('header-red')) {
+            $('.tab-pane').removeClass('show');
+            $('#audit_ad_img').addClass('show');
+        } else if ($('#audit_sales').hasClass('header-red')) {
+            $('.tab-pane').removeClass('show');
+            $('#audit_sales_img').addClass('show');
+        } else if ($('#audit_site').hasClass('header-red')) {
+            $('.tab-pane').removeClass('show');
+            $('#audit_site_img').addClass('show');
+        }
     });
 
 
     //E-mail Ajax Send
     //Форма заказа
-	$("#order").submit(function() {
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "http://localhost:8888/mail.php",
-			data: th.serialize()
-		}).done(function() {
-            dataLayer.push({'event': 'formzayavka1'});
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+    $("#order").submit(function() {
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8888/mail.php",
+            data: th.serialize()
+        }).done(function() {
+            dataLayer.push({
+                'event': 'formzayavka1'
+            });
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
 
-    //Скачать презентацию
-    $("#downloadPresentation").submit(function() {
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "http://localhost:8888/mail.php",
-			data: th.serialize()
-		}).done(function() {});
-
-	});
 
     //Скачать прайс
     $("#downloadPrice").submit(function() {
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "http://localhost:8888/mail.php",
-			data: th.serialize()
-		}).done(function() {});
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8888/mail.php",
+            data: th.serialize()
+        }).done(function() {});
 
-	});
+    });
 
     //заявка на аудит
     $("#auditForm").submit(function() {
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "http://localhost:8888/mail.php",
-			data: th.serialize()
-		}).done(function() {
-            dataLayer.push({'event': 'formzayavka2'});
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8888/mail.php",
+            data: th.serialize()
+        }).done(function() {
+            dataLayer.push({
+                'event': 'formzayavka2'
+            });
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
 
     //FAQ
     $("#faqForm").submit(function() {
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "http://localhost:8888/mail.php",
-			data: th.serialize()
-		}).done(function() {
-            dataLayer.push({'event': 'formzayavka2'});
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8888/mail.php",
+            data: th.serialize()
+        }).done(function() {
+            dataLayer.push({
+                'event': 'formzayavka2'
+            });
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/bootstrap/bootstrap.js',
+		'app/libs/revealator-master/fm.revealator.jquery.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
